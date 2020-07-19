dd475615318a01fd2ee444f9bde16ab8f72b4c96
talforsher1@gmail.com <talforsher1@gmail.com>
2020-07-19 12:13:47 +0300

    Auto Deploy

--- public/edit.js
+++ public/edit.js
@@ -276,14 +276,7 @@ function save() {
 }
 var saveButton = document.createElement('button')
 saveButton.innerText = "Save"
-saveButton.style = `
-position: absolute;
-width: 100%;
-background: #0000000d;
-color: black;
-border-width: 8px;
-border-style: solid;
-border-color: #ff000014;`
+saveButton.style = "position: absolute; width:100%"
 saveButton.onclick = save;
 document.body.appendChild(saveButton);
 
